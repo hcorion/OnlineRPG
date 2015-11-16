40ca5b02924246a6da7b2825a5382752792b903a
Zion Nimchuk <zionnimchuk@gmail.com>
2015-11-15 18:44:25 -0800

    Moved javascript code to the javascript.js file. Cleaned up functions.

--- javascript.js
+++ javascript.js
@@ -1,70 +1,111 @@
-  //Log In Data
-  var username;
-  var password;
-  var accountIndex;
-  //Parsing the userdata .json file located in the root directory.
-  var mydata = JSON.parse(userdata);
-  
-  //reading the data XML file using Jquery
-  var name;
+//Log In Data
+var username;
+var password;
+var accountIndex;
+
+//Used to store all the locations titles.
+var locationTitle = new Array();
+var locationDesc = new Array();
+var locationImage = new Array();
+
+//Parsing the userdata .json file located in the root directory.
+var mydata = JSON.parse(userdata);
+
+//Preparing the XML file for easier parsing
+var TextData;
 $.ajax({
-  url: 'TextData.xml',
-  async : false,
-  dataType: "xml",
-  success: function(xml) {
-			$(xml).find('main').each(function()
-			{
-                $(this).find("data").each(function()
-				{
-					$(this).find("spawnTown").each(function()
-					{
-						name = $(this).text();
-						alert(name);
-					});
-				});
-			});
-		}
-	});
-	var DaXML;
-	$.ajax({
-	url: 'TextData.xml',
-	async : false,
-	dataType: "xml",
-	success: function(xml) 
-	{
-		$(xml).find('main').each(function()
-		{
-			DaXML = $(this);
-		});
-	}
-	});
-  //Called by LogIn() after successful log in.
-  function initWorld()
-  {
-	document.getElementById("description").innerHTML = "Welcome to OnlineRPG, " + username + "! It's currently still in development.";
-	document.getElementById("location").innerHTML = "Home Page";
-	document.getElementById("location").innerHTML = name;
-	
-	//Get world startingTitle
-	var desc;
-	$(DaXML).find("locations").each(function(){$(this).find("location").each(function(){$(this).find("startingTitle").each(function(){desc = $(this).text();});});});
-	document.getElementById("description").innerHTML = desc.replace("{username}", username);
-	var image;
-	$(DaXML).find("locations").each(function(){$(this).find("location").each(function(){$(this).find("Image").each(function(){image = $(this).text();});});});
-	document.getElementById("preview").src="./Images/Locations/" + image;
-  }
-  
-  //Called after you press the Log In Button
-  function LogIn() {
-	for (i = 0, len = mydata.length; i < len; i++)
-	{
-		if (document.getElementById('uname').value == mydata[i].username && document.getElementById('pswrd').value == mydata[i].password)
-		{
-			username = mydata[i].username;
-			password = mydata[i].pswrd;
-			accountIndex = i;
-			initWorld();
-			break;
-		}
-	}
-  }
+    url: 'TextData.xml',
+    async: false,
+    dataType: "xml",
+    success: function (xml) {
+        $(xml).find('main').each(function () {
+            TextData = $(this);
+        });
+    }
+});
+//Loading the location data and parsing it into arrays.
+$(TextData).find("locations").each(function () {
+        $(this).find("location").each(function () {
+
+            locationTitle.push($("name", this).text());
+            locationDesc.push($("startingTitle", this).text());
+            locationImage.push($("Image", this).text());
+        });
+    });
+//Called by LogIn() after successful log in.
+function initWorld() {
+    
+    var spawnTown;
+    $(TextData).find("data").each(function (){
+        $(this).find("spawnTown").each(function (){
+            spawnTown = ($("name", this).text());
+        });
+    });
+    
+    
+    //Get the starting location and load it.
+    var CorrectIndex;
+    for (i = 0; i < locationTitle.length; i++) {
+        if (locationTitle[i] == spawnTown) {
+            CorrectIndex = i;
+            LoadLocation(locationTitle[i]);
+            
+        }
+    }
+}
+
+//Called after you click a link
+function LoadLocation(location) {
+    //Preparing things
+    var DescLocationName = new Array();
+    var DescLocationTitleName = new Array();
+    var placesToGoText = " ";
+    //Querying the database to load the Description and Image.
+    
+
+
+    for (i = 0; i < locationTitle.length; i++) {
+        //For making sure were going to a valid location.
+        if (locationTitle[i] == location) {
+            $(TextData).find("locations").each(function () {
+                $(this).find("location").each(function () {
+                    if (locationTitle[i] == $('name', this).text()) {
+                        $(this).find("toLocations").each(function () {
+                            $(this).find("toLocation").each(function () {
+                                DescLocationName.push($('displayName', this).text());
+                                DescLocationTitleName.push($('titleName', this).text());
+                            });
+                        });
+                    }
+                });
+            });
+
+            //Assembling the placesToGoText
+            for (p = 0; p < DescLocationName.length; p++) {
+                placesToGoText += '<a onclick="LoadLocation(' + "'" + DescLocationTitleName[p] + "'" + ');" href="#">' + DescLocationName[p] + '</a><br />';
+            }
+            //Setting the description of the location.
+            document.getElementById("description").innerHTML = "<p>" + locationDesc[i].replace("{username}", username) + "</p><br />" +
+                "<h5>Places To Go</h5><p>" + placesToGoText + "</p>";
+            //Setting the Title and image.
+            document.getElementById("location").innerHTML = locationTitle[i];
+            document.getElementById("preview").src = "./Images/Locations/" + locationImage[i];
+            return 0;
+        }
+    }
+    alert("Uh oh! We don't have that location in our database!");
+}
+
+//Called after you press the Log In Button
+function LogIn() {
+    for (i = 0, len = mydata.length; i < len; i++) {
+        if (document.getElementById('uname').value == mydata[i].username && document
+            .getElementById('pswrd').value == mydata[i].password) {
+            username = mydata[i].username;
+            password = mydata[i].pswrd;
+            accountIndex = i;
+            initWorld();
+            break;
+        }
+    }
+}
